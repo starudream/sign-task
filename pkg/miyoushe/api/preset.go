259e3c1c2f6bf5646783e73e4dcf0f92def29a77
sheng.wang <justwangsheng@gmail.com>
2025-02-08 23:12:43 +0800

    chore: miyoushe 2.81.1

--- pkg/miyoushe/api/preset.go
+++ pkg/miyoushe/api/preset.go
@@ -11,7 +11,7 @@ const (
 	RefererApp = "https://app.mihoyo.com"
 	RefererAct = "https://act.mihoyo.com"
 
-	AppVersion    = "2.73.1"
+	AppVersion    = "2.81.1"
 	AppIdMiyoushe = "bll8iq97cem8" // 米游社
 
 	UserAgent = "Mozilla/5.0 (Linux; Android 13; 22011211C Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 miHoYoBBS/" + AppVersion
